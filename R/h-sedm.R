9f127a8e36b2de8f5864a00f7fa24652022aaca4
annakrystalli <annakrystalli@googlemail.com>
2020-03-27 09:20:20 +0000

    rename sedmaps functions

--- R/h-sedm.R
+++ R/h-sedm.R
@@ -5,7 +5,7 @@
 #'
 #' @return a tibble of extracted layer data
 #' @export
-get_sedmaps <- function(obis_recs, layers = names(sm_rst)){
+get_sedm <- function(obis_recs, layers = names(sm_rst)){
     checkmate::assert_subset(layers, names(sm_rst))
     
     raster::extract(sm_rst[[layers]], obis_recs) %>% 
