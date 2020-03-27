13a8d356ef5facf121f33be068cc0613a69b825c
annakrystalli <annakrystalli@googlemail.com>
2020-03-27 08:59:35 +0000

    add pipe

---
+++ R/utils-pipe.R
@@ -0,0 +1,23 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Compound assignment pipe-operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
