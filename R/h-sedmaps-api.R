f7f3fbad58521fa291afcbd6c7115c83c03635ad
annakrystalli <annakrystalli@googlemail.com>
2020-03-27 08:58:05 +0000

    add sedmaps api

---
+++ R/h-sedmaps-api.R
@@ -0,0 +1,13 @@
+#' Extract sedmaps data for an OBIS occurence sf
+#'
+#' @param obis_recs OBIS occurence sf
+#' @param layers character vector of layer names to extract
+#'
+#' @return a tibble of extracted layer data
+#' @export
+get_sedmaps <- function(obis_recs, layers = names(sm_rst)){
+    checkmate::assert_subset(layers, names(sm_rst))
+    
+    raster::extract(sm_rst[[layers]], obis_recs) %>% 
+        tibble::as_tibble()
+}
