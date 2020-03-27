65b8efb0381e706bd4bb2048b0f094353c9bb134
annakrystalli <annakrystalli@googlemail.com>
2020-03-27 08:59:14 +0000

    add obis functionality

--- R/obis-api.R
+++ R/obis-api.R
@@ -1,81 +1,109 @@
-#' Title
+#' Query OBIS for occurence data
 #'
-#' @param species_id
-#' @param missing_check
-#' @param fields
+#' @param sp_id a valid WoRMS aphiaID
+#' @param missing_check whether to check for validity of aphiaID on OBIS
+#' @param fields fields to return from OBIS query. Defaults to `c("decimalLongitude", 
+#' "decimalLatitude","minimumDepthInMeters", "maximumDepthInMeters", "depth",
+#' "eventDate", "year", "month", "scientificName", "aphiaID")`
 #'
-#' @return
+#' @return a tibble of temporally and spatially located OBIS occurence records 
+#' for `sp_id`.
 #' @export
 #'
 #' @examples
-get_obis_recs <- function(species_id, missing_check = FALSE,
+#' get_obis_recs(sp_id = 325567)
+get_obis_recs <- function(sp_id, missing_check = FALSE,
                           fields = c(
                             "decimalLongitude", "decimalLatitude",
                             "minimumDepthInMeters", "maximumDepthInMeters", "depth",
                             "eventDate", "year", "month",
                             "scientificName", "aphiaID"
-                          )) {
-  # Fuction to get OBIS records for a given species_id, which must be a recognised WoRMS Aphia ID
+                          ), trim = NULL) {
+  # Fuction to get OBIS records for a given sp_id, which must be a recognised WoRMS Aphia ID
 
   # NB OBIS returns records from all taxa gathered under the same valid Aphia ID; the aphia ID returned is that of the taxon as recorded, not necessarily the valid ID, so in order that the final dataset is correctly named we add back in the 'correct' ID here as valid_AphiaID
 
+  fields <- match.arg(fields, c(
+    "decimalLongitude", "decimalLatitude",
+    "minimumDepthInMeters", "maximumDepthInMeters", "depth",
+    "eventDate", "year", "month",
+    "scientificName", "aphiaID"
+  ), several.ok = TRUE)
+
   if (missing_check == TRUE) {
     # catch invalid / unrecognised AphiaIDs here - but recommend doing this prior to calling these functions
-    if (length(checklist(taxonid = species_id)) > 1) {
-      # get OBIS records for a given species ID, add year and month, set negative and missing depth to 0
+    if (length(robis::checklist(taxonid = sp_id)) > 1) {
+      # get OBIS records for a given species ID, add year and month,
       obis_recs <- robis::occurrence(
-        taxonid = species_id, fields = fields
+        taxonid = sp_id, fields = fields
       ) %>%
-        as_tibble()
-      if (!"year" %in% names(obis_recs)) {
-        obis_recs <- obis_recs %>%
-          mutate(year = "NA")
-      }
-      if (!"month" %in% names(obis_recs)) {
-        obis_recs <- obis_recs %>%
-          mutate(month = NA)
-      }
-      obis_recs <- mutate(obis_recs,
-        depth = as.numeric(depth),
-        year = formatC(year),
-        month = formatC(as.numeric(month), width = 2, flag = "0"),
-        depth0 = case_when(
-          is.na(depth) ~ 0,
-          depth < 0 ~ 0,
-          TRUE ~ depth
-        ),
-        valid_AphiaID = species_id
-      )
+        obis_recs_process()
     } else {
       # at present just returns an empty tibble, which causes problems with other functions further down the pipeline, hence recommend checking AphiaIDs prior to calling
-      obis_recs <- tibble()
+      obis_recs <- tibble::tibble()
     }
   } else {
     obis_recs <- robis::occurrence(
-      taxonid = species_id, fields = fields
+      taxonid = sp_id, fields = fields
     ) %>%
-      as_tibble()
-    if (!"year" %in% names(obis_recs)) {
-      obis_recs <- obis_recs %>%
-        mutate(year = "NA")
-    }
-    if (!"month" %in% names(obis_recs)) {
-      obis_recs <- obis_recs %>%
-        mutate(month = NA)
-    }
-    obis_recs <- mutate(
-      obis_recs,
-      depth = as.numeric(depth),
-      year = formatC(year),
-      month = formatC(as.numeric(month), width = 2, flag = "0"),
-      depth0 = case_when(
-        is.na(depth) ~ 0,
-        depth < 0 ~ 0,
-        TRUE ~ depth
-      ),
-      valid_AphiaID = species_id
-    )
+      obis_recs_process() %>%
+      dplyr::mutate(valid_AphiaID = sp_id)
   }
   # return the OBIS records
   obis_recs
 }
+
+
+
+obis_recs_process <- function(obis_recs) {
+  obis_recs <- tibble::as_tibble(obis_recs)
+
+  if (!"year" %in% names(obis_recs)) {
+    obis_recs <- obis_recs %>%
+      dplyr::mutate(year = "NA")
+  }
+  if (!"month" %in% names(obis_recs)) {
+    obis_recs <- obis_recs %>%
+      dplyr::mutate(month = NA)
+  }
+
+  dplyr::mutate(
+    obis_recs,
+    depth = as.numeric(.data$depth),
+    year = formatC(.data$year),
+    month = formatC(as.numeric(.data$month), width = 2, flag = "0"),
+    # set negative and missing depth to 0
+    depth0 = dplyr::case_when(
+      is.na(.data$depth) ~ 0,
+      .data$depth < 0 ~ 0,
+      TRUE ~ .data$depth
+    )
+  )
+}
+
+
+checkmate_crs <- function(crs){
+  if(checkmate::test_null(crs)){
+    crs <- 4326
+    usethis::ui_warn("{usethis::ui_field('crs')} missing. Set to default {usethis::ui_value(crs)}")
+  }
+  crs
+}
+
+#' @export
+checkmate_obis_recs <- function(obis_recs, crs = NULL){
+  if(!checkmate::test_class(obis_recs, "sf")){
+    crs <- checkmate_crs(crs)
+    obis_recs %<>% sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+                                crs = crs)
+    usethis::ui_done("{usethis::ui_code('obis_recs')} successfully converted to {usethis::ui_field('sf')}")
+  }
+  if(is.na(crs(obis_recs)) | is.null(crs(obis_recs))){
+    crs <- checkmate_crs(crs)
+    obis_recs %<>% sf::st_set_crs(crs)
+    usethis::ui_done("{usethis::ui_code('obis_recs')} {usethis::ui_field('crs')} successfully set")
+  }
+  usethis::ui_done("{usethis::ui_code('obis_recs')} {usethis::ui_field('crs')}: {usethis::ui_value(sf::st_crs(obis_recs))}")
+  obis_recs
+}
+
--- R/obis-to-h.R
+++ R/obis-to-h.R
@@ -1,96 +1,157 @@
 #' Title
 #'
+#' function to get OBIS records for a given species and match to IAP and Bio-Oracle temperatures
+#' if save_all_recs == TRUE, this full matched dataset will be saved before summarising
+#' function returns a summary of temperature affinity of the species
+#'
 #' @param sp_id
-#' @param save_all_recs
 #' @param check_match
-#' @param use_defaults
+#' @param layers character vector of habitat layers to match
+#' 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-get_temp_summ_by_sp <- function(sp_id, save_all_recs = TRUE, check_match = TRUE, use_defaults = TRUE) {
-  # function to get OBIS records for a given species and match to IAP and Bio-Oracle temperatures
-  # if save_all_recs == TRUE, this full matched dataset will be saved before summarising
-  # function returns a summary of temperature affinity of the species
+match_temp_sp <- function(sp_id, layers,
+                          #save_all_recs = TRUE, 
+                          check_match = FALSE,
+                          trim = NULL) {
+ 
+    if (check_match == TRUE) {
+    # TODO
+    }
+    obis_recs <- get_obis_recs(sp_id = sp_id, trim = trim()) %>%
+      checkmate_obis_recs(crs = 4326) %>%
+      #get_iap_gridded_t() %>%
+      get_bio_oracle_t() %>%
+      #save_full_recs(save_recs = save_all_recs) %>%
+      #t_summary() %>%
+        dplyr::mutate(species_id = .env$sp_id) %>%
+      dplyr::select(.data$species_id, dplyr::everything())
+      
+      
+ 
 
-  # First, check if this species has previously been matched, if check_match is TRUE
-  # (This is an option in case the user wants to update all records)
+  sp_temp
+}
 
-  if (check_match == TRUE) {
+
+# First, check if this species has previously been matched, if check_match is TRUE
+# (This is an option in case the user wants to update all records)
+
+check_cached_matches <- function(use_defaults = TRUE, out_path){
+
+
     out_path <- ifelse(use_defaults, paste0(file.path(getwd()), "/t_matched_obis_recs"),
-      paste0(bespoke_path, "/t_matched_obis_recs")
+                       paste0(bespoke_path, "/t_matched_obis_recs")
     )
     # set species_done flag to FALSE
     species_done <- FALSE
-
+    
     # check if the directory exists:
     if (dir.exists(file.path(out_path))) {
-      # get list of aphia ids from the files in this directory:
-      done_files <- list.files(path = out_path)
-      done_aphias <- done_files %>%
-        word(sep = "_") %>%
-        str_replace(pattern = "aphia", replacement = "") %>%
-        as.integer()
-
-      # check if species sp_id is in this list
-      species_done <- sp_id %in% done_aphias
+        # get list of aphia ids from the files in this directory:
+        done_files <- list.files(path = out_path)
+        done_aphias <- done_files %>%
+            word(sep = "_") %>%
+            str_replace(pattern = "aphia", replacement = "") %>%
+            as.integer()
+        
+        # check if species sp_id is in this list
+        species_done <- sp_id %in% done_aphias
     }
-
+    
     # get the file if it exists
     if (species_done == TRUE) {
-      which_file <- which(done_aphias == sp_id)
-
-      # what if more than one file matches?
-      if (length(which_file) > 1) {
-        # find the most recent one
-        which_file <- which_file[order(done_files[which_file], decreasing = TRUE) == 1]
-      }
-
-      sp_temp <- read_csv(
-        file = paste(out_path, done_files[which_file], sep = "/"),
-        col_types = cols(
-          decimalLongitude = col_double(),
-          decimalLatitude = col_double(),
-          depth = col_double(),
-          eventDate = col_datetime(format = ""),
-          scientificName = col_character(),
-          aphiaID = col_integer(),
-          year = col_integer(),
-          month = col_character(),
-          depth0 = col_double(),
-          grid_depth = col_integer(),
-          grid_lon = col_integer(),
-          grid_lat = col_integer(),
-          iap_t = col_double(),
-          iap_sst = col_double(),
-          iap_sbt = col_double(),
-          iap_grid_bottom_depth = col_integer(),
-          bo_sst = col_double(),
-          bo_sbt = col_double()
-        )
-      ) %>%
-        t_summary() %>%
-        mutate(species_id = sp_id) %>%
-        dplyr::select(species_id, everything())
+        which_file <- which(done_aphias == sp_id)
+        
+        # what if more than one file matches?
+        if (length(which_file) > 1) {
+            # find the most recent one
+            which_file <- which_file[order(done_files[which_file], decreasing = TRUE) == 1]
+        }
+        
+        sp_temp <- read_csv(
+            file = paste(out_path, done_files[which_file], sep = "/"),
+            col_types = cols(
+                decimalLongitude = col_double(),
+                decimalLatitude = col_double(),
+                depth = col_double(),
+                eventDate = col_datetime(format = ""),
+                scientificName = col_character(),
+                aphiaID = col_integer(),
+                year = col_integer(),
+                month = col_character(),
+                depth0 = col_double(),
+                grid_depth = col_integer(),
+                grid_lon = col_integer(),
+                grid_lat = col_integer(),
+                iap_t = col_double(),
+                iap_sst = col_double(),
+                iap_sbt = col_double(),
+                iap_grid_bottom_depth = col_integer(),
+                bo_sst = col_double(),
+                bo_sbt = col_double()
+            )
+        ) %>%
+            t_summary() %>%
+            mutate(species_id = sp_id) %>%
+            dplyr::select(species_id, everything())
     } else {
-      sp_temp <- get_obis_recs(species_id = sp_id) %>%
-        get_iap_gridded_t() %>%
-        get_bio_oracle_t() %>%
-        save_full_recs(save_recs = save_all_recs) %>%
-        t_summary() %>%
-        mutate(species_id = sp_id) %>%
-        dplyr::select(species_id, everything())
+        sp_temp <- get_obis_recs(sp_id = sp_id) %>%
+            get_iap_gridded_t() %>%
+            get_bio_oracle_t() %>%
+            save_full_recs(save_recs = save_all_recs) %>%
+            t_summary() %>%
+            mutate(species_id = sp_id) %>%
+            dplyr::select(species_id, everything())
     }
-  } else {
-    sp_temp <- get_obis_recs(species_id = sp_id) %>%
-      get_iap_gridded_t() %>%
-      get_bio_oracle_t() %>%
-      save_full_recs(save_recs = save_all_recs) %>%
-      t_summary() %>%
-      mutate(species_id = sp_id) %>%
-      dplyr::select(species_id, everything())
-  }
+} 
 
-  sp_temp
+
+oh_mtch_wide <- function(vars ){
+tibble::tibble(
+    decimalLongitude = readr::col_double(),
+    decimalLatitude = readr::col_double(),
+    depth = readr::col_double(),
+    eventDate = readr::col_datetime(format = ""),
+    scientificName = readr::col_character(),
+    aphiaID = readr::col_integer(),
+    year = readr::col_integer(),
+    month = readr::col_character(),
+    depth0 = readr::col_double(),
+    grid_depth = readr::col_integer(),
+    grid_lon = readr::col_integer(),
+    grid_lat = readr::col_integer(),
+    iap_t = readr::col_double(),
+    iap_sst = readr::col_double(),
+    iap_sbt = readr::col_double(),
+    iap_grid_bottom_depth = readr::col_integer(),
+    bo_sst = readr::col_double(),
+    bo_sbt = readr::col_double()
+)
+}
+
+
+
+
+oh_mtch_wide <- function(vars, config ){
+    tibble::tibble(
+        decimalLongitude = readr::col_double(),
+        decimalLatitude = readr::col_double(),
+        depth = readr::col_double(),
+        eventDate = readr::col_datetime(format = ""),
+        scientificName = readr::col_character(),
+        aphiaID = readr::col_integer(),
+        year = readr::col_integer(),
+        month = readr::col_character(),
+        depth0 = readr::col_double(),
+        grid_depth = readr::col_integer(),
+        grid_lon = readr::col_integer(),
+        grid_lat = readr::col_integer(),
+        h_name = readr::col_double(),
+        h_value = readr::col_double(),
+
+    )
 }
