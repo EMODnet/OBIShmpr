e00f004219346e5587d81268639ceff772672386
annakrystalli <annakrystalli@googlemail.com>
2020-06-18 16:47:38 +0100

    Remove ported emodnet functions

--- R/h-emodnet.R
+++ R/h-emodnet.R
@@ -1,12 +1,11 @@
 
-
-
 #' Access EMODnet EUNIS Seabed Habitat layers
 #'
 #' @param layers character vector of EMODnet seabed habitat wfs layer names.
 #' @param cache_dir path to the cached data directory. Defaults to the "cached-data' 
 #' folder in the root of the current project
 #' @param wfs_url url to the EMODnet WFS service. Defaults to the EUNIS seabed habitat map wfs.
+#' @param crs EPSG crs code to reproject to. Defaults to 4326
 #'
 #' @return an sf of requested features
 #' @export
@@ -15,24 +14,30 @@
 #' proj_dir <- tempdir()
 #' cache_dir <- fs::path(proj_dir, "cached-data")
 #' get_emodnet_wfs_layers(layers = "be000142", cache_dir = cache_dir)
-get_emodnet_wfs_layers <- function(layers = c(
-                                 "be000142", "be000143", "be000144", "bg003005",
-                                 "es000001", "es001000"
-                               ),
+get_emodnet_wfs_layers <- function(layers,
                                cache_dir = NULL,
-                               wfs_url = "https://ows.emodnet-seabedhabitats.eu/emodnet_open_maplibrary/wfs") {
-  layers <- match.arg(layers, several.ok = TRUE)
+                               wfs_url = "https://ows.emodnet-seabedhabitats.eu/emodnet_open_maplibrary/wfs",
+                               crs = 4326,
+                               overwrite = FALSE) {
+  layers <- match.arg(layers, choices = layer_info$layer_code,
+                      several.ok = TRUE)
   if (is.null(cache_dir)) {
     cache_dir <- here::here("cached-data")
   }
   fs::dir_create(cache_dir)
   layer_paths <- fs::path(cache_dir, glue::glue("emodnet_{layers}.geojson"))
-
+  wfs_url <- paste0("WFS:", wfs_url)
+  
   # checker whether layers are cached. If not, download from WFS into cache_dir
+  if(overwrite){
+      file.check <- rep(TRUE, length(layers))
+  }else{
   file.check <- !fs::file_exists(layer_paths)
-
+  }
   if (any(file.check)) {
-    purrr::map2(
+   
+    emodnet_get_wfs_info()  
+      purrr::map2(
       .x = layers[file.check], .y = layer_paths[file.check],
       ~ gdalUtils::ogr2ogr(
         src_datasource_name = wfs_url,
@@ -41,13 +46,19 @@ get_emodnet_wfs_layers <- function(layers = c(
         f = "geojson", # the target format
         # spat = c(left = 142600, bottom = 153800, right = 146000, top = 156900),
         # the bounding box
-        # t_srs = "EPSG:31370",                    # the coordinate reference system
-        verbose = TRUE
+        #t_srs = glue::glue("EPSG:{crs}"),                    # the coordinate reference system
+        verbose = TRUE,
+        overwrite = TRUE
+        #append = TRUE
       )
     )
+      usethis::ui_done("Download of EMODnet {usethis::ui_field('layers')} {usethis::ui_value(layers)} complete")
+      usethis::ui_info("{usethis::ui_field('layers')} cached in {usethis::ui_field('cache_dir')} {usethis::ui_path(cache_dir)}")
+      
   }
 
   # Load layers
+  usethis::ui_info("Reading {usethis::ui_field('layers')} from {usethis::ui_field('cache_dir')} {usethis::ui_code('cache_dir'); usethis::ui_path(cache_dir)}")
   purrr::map(
     layer_paths,
     ~ sf::st_read(.x) %>%
@@ -56,24 +67,3 @@ get_emodnet_wfs_layers <- function(layers = c(
   ) %>% do.call("rbind", .)
 }
 
-
-
-
-#' Access EMODnet WFS info
-#'
-#' @inheritParams get_emodnet_wfs_layers
-#'
-#' @return WFS available feature information
-#' @export
-#'
-#' @examples
-#' get_emodnet_info()
-get_emodnet_info <- function(wfs_url = "https://ows.emodnet-seabedhabitats.eu/emodnet_open_maplibrary/wfs") {
-  wfs_url <- paste0("WFS:", wfs_url)
-  info <- gdalUtils::ogrinfo(wfs_url,
-    so = TRUE,
-    ro = TRUE
-  )
-
-  info
-}
